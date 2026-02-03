ea49ac36c3225b67570d34631925a508878d59ae
jeyasuriyak25 <jeyasuriyak25@gmail.com>
2026-02-03 15:47:58 +0530

    Datatypes and Variables
    completed

---
+++ datatypes/Chrome.java
@@ -0,0 +1,19 @@
+package datatypes;
+
+public class Chrome {
+    public static void main(String[] args){
+        float version=91.0f;
+        String versionType="Google";
+        boolean isBeta=false;
+        int year=2008;
+        char key='c';
+
+        System.out.println("Version is "+version);
+        System.out.println("Version Type is "+versionType);
+        System.out.println("Is Beta "+isBeta);
+        System.out.println("Year is "+year);
+        System.out.println("Key is "+key);
+    }
+
+
+}
